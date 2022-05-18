d9d7f0f9297a354bdfba536fd78a1cbde3c1e7e8
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-05-18 13:35:29 +0200

    Update main.cpp

--- Programowanie Obiektowe/*Materiały*/Kontener tablicy/main.cpp
+++ Programowanie Obiektowe/*Materiały*/Kontener tablicy/main.cpp
@@ -13,3 +13,26 @@ liczby.push_back(4);
 for (int i=0;i<liczby.size();i++ ){
     cout << liczby[i]<< endl;
 }
+
+// Sortowanie vectora
+
+vector<int> liczby {4,2,3,8,1};
+sort(liczby.begin(),liczby.end());
+for (int a:liczby){
+    cout << a << " ";
+}
+
+// Pętla zakresowa
+
+vector<int> liczby {1,2,3};
+
+for (int a:liczby){
+    cout << a << " ";
+}
+cout << endl;
+
+liczby.push_back(4);
+
+for (int a:liczby){
+    cout << a << " ";
+}
