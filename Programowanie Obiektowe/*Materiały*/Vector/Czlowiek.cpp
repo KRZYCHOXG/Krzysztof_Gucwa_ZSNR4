13a886a64b9d0dcc5b707243e4babcc46e23ee6b
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-05-18 13:46:45 +0200

    Add files via upload

---
+++ Programowanie Obiektowe/*Materiały*/Vector/Czlowiek.cpp
@@ -0,0 +1,11 @@
+#include "Czlowiek.h"
+
+Czlowiek::Czlowiek()
+{
+    //ctor
+}
+
+Czlowiek::~Czlowiek()
+{
+    //dtor
+}
---
+++ Programowanie Obiektowe/*Materiały*/Vector/Czlowiek.h
@@ -0,0 +1,16 @@
+#ifndef CZLOWIEK_H
+#define CZLOWIEK_H
+
+
+class Czlowiek
+{
+    public:
+        Czlowiek();
+        virtual ~Czlowiek();
+
+    protected:
+
+    private:
+};
+
+#endif // CZLOWIEK_H
