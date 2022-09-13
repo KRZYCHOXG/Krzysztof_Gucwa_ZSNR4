517007064339965e72bb96f6bff6d3542a379400
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-13 08:41:42 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Zmienne i stałe/ZD1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne i stałe</title>
+</head>
+<body>
+    <?php
+    $zloto = 18000; // Polskie złotówki
+    $dolar = 4.63; // Kurs $
+    $wartosc = $zloto/$dolar; // Wzór
+    echo 'Ilość PLN to wynosi '.$wartosc.' w $ (dolarach)';
+    ?>
+</body>
+</html>
