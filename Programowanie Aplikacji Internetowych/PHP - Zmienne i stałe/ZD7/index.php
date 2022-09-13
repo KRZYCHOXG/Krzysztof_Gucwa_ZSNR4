3a543681ec61ee1aea93b825d2a6d929b197cc45
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-13 08:38:41 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Zmienne i stałe/ZD7/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne i stałe</title>
+</head>
+<body>
+    <?php
+    $kilo = 30; // 30 kilogramów
+    $funty = 2.204; // Kurs funta
+    $masa = $kilo*$funty; // Wzór
+    echo 'Ilość kilogramów z funtów to '.$masa;
+    ?>
+</body>
+</html>
