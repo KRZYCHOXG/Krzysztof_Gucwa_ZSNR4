68262de440cf1641558551c83d04d4231b7c1296
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-27 07:49:05 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Funkcje wbudowane/ZD2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funkcja wbudowanet</title>
+</head>
+<body>
+    <?php  
+        $str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget sem vitae sapien posuere tincidunt. Nulla facilisi. Nulla ac ipsum convallis, volutpat nulla vel, euismod ante. Ut volutpat ante lectus, non consectetur urna placerat ut. Curabitur fermentum, libero a auctor consectetur, sapien mi gravida lacus, vitae dignissim nulla magna ac ligula. Maecenas rutrum elementum finibus. Vestibulum ornare pharetra nulla sit amet feugiat. Vestibulum aliquet efficitur urna, eget egestas odio pulvinar non. Sed ullamcorper lacinia eros, in accumsan augue imperdiet quis. Suspendisse imperdiet, metus id iaculis lobortis, felis urna maximus dolor, id rutrum mi turpis et lorem. Mauris mauris erat, bibendum sed lectus vel, condimentum fringilla mauris Dr. Livesey.
+
+        Integer eget ex molestie, faucibus diam vel, accumsan ex. Phasellus hendrerit condimentum magna eu pellentesque. Mauris viverra nunc id blandit rhoncus. Nulla facilisi. Maecenas faucibus venenatis leo in pulvinar. Fusce sed lacinia felis. Vestibulum orci libero, faucibus vitae tempus sit amet, porttitor quis ligula. Praesent convallis purus et luctus dignissim.
+        
+        Vivamus at tempus ante, eget dapibus est. Donec tortor dolor, condimentum sit amet lectus non, cursus porttitor justo. Fusce tincidunt iaculis ipsum. Fusce vel pretium urna, sed fringilla diam. Vestibulum vehicula nisi purus, sit amet semper urna fringilla et. Praesent semper enim convallis ipsum tincidunt, scelerisque pellentesque sem tincidunt. Aliquam diam sapien, lacinia eu maximus in, sollicitudin nec dui. Nunc rhoncus massa arcu, quis feugiat diam hendrerit vel. Nam consequat turpis eu erat faucibus finibus.
+        
+        Ut tincidunt ipsum eget turpis fermentum egestas. Nam nulla libero, suscipit eget nisl nec, pulvinar vestibulum mi. Cras sagittis vitae mauris non lacinia. Vivamus accumsan quam nec lacus tristique lacinia. Pellentesque hendrerit laoreet dolor, at semper lectus porta efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent justo turpis, malesuada nec efficitur vel, fringilla ac dui. Praesent vitae mattis diam, tincidunt tincidunt metus.
+        
+        Phasellus sed egestas justo. Sed tempor at dui ac feugiat. Nunc ac metus bibendum, convallis magna ut, viverra ligula. Duis efficitur dolor id lobortis rutrum. Etiam porta malesuada quam, sit amet egestas sem laoreet et. Sed ultricies, quam a mattis sagittis, urna est auctor purus, euismod finibus turpis nunc in quam. Maecenas enim magna, mattis eu pharetra eu, placerat ut justo. Morbi venenatis in arcu ac pretium. Phasellus ornare lacus ligula, eget condimentum sapien volutpat eu.';
+        echo strlen($str); 
+    ?>
+</body>
+</html>
