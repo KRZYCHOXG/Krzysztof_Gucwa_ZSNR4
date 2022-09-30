a20ded99fc5c9d6cbea15bacfd0830bd0c469341
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 10:04:32 +0200

    Update index.php

--- Programowanie Aplikacji Internetowych/PHP - Tablice/ZD0/index.php
+++ Programowanie Aplikacji Internetowych/PHP - Tablice/ZD0/index.php
@@ -18,21 +18,13 @@
 <body>
     <?php  
         header("refresh: 0.3");
-        /* echo '<h1 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h1>';
-        $date = "2023-04-03 19:00:00";
-        $teraz = new DateTime(date('Y-m-d'));
-        $urodziny = new DateTime('2023-04-03');
-        $obliczenie = $teraz->diff($urodziny);
-        echo '<br>';
-        echo '<h1> Urodziny mam za ';
-        echo $obliczenie->format('%m miesiecy i %d dni');
-        echo '<br>'.$date; */
-        echo '<h1 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h1>';
+        echo '<h2 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h2>';
         $liczby = array();
         for($i = 0; $i < 30; $i++) {
             $liczby[$i] = rand(1, 29);
         }
-        echo implode(', ', $liczby);
+        echo implode(', ', $liczby).'<br>';
+        echo '<h3 style="text-transform:uppercase;">Luks</h3>';
         ?>
 </body>
 </html>
