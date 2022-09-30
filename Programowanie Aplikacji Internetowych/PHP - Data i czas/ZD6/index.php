b433a21b9bdb69f2535be4753175a144fb6f5695
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 08:41:13 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD6/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data i czas</title>
+    <style>
+        * {
+            font-family: 'Arial';
+            text-align: center;
+            color: white;
+            padding: 15px;
+            background-color: rgb(19,19,19);
+        }
+    </style>
+</head>
+<body>
+    <?php  
+        header("refresh: 1");
+        echo '<h1 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h1>';
+        $teraz = new DateTime(date('Y-m-d'));
+        $urodziny = new DateTime('2023-04-03');
+        $obliczenie = $teraz->diff($urodziny);
+        echo '<br>';
+        echo '<h1> Urodziny mam za ';
+        echo $obliczenie->format('%m miesiecy i %d dni');
+    ?>
+</body>
+</html>
