7bef9d7f94d95d0615e7664d58274ebe588ec99b
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 08:42:28 +0200

    Update index.php

--- Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD2/index.php
+++ Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD2/index.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data i czas</title>
+    <style>
+        * {
+            font-family: 'Arial';
+            text-align: center;
+            color: white;
+            padding: 15px;
+            background-color: rgb(19,19,19);
+        }
+    </style>
 </head>
 <body>
     <?php  
