86ac27d7a5cc37c8b0861e7727ab3bd40eabd7dd
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 08:35:05 +0200

    Update index.php

--- Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD4/index.php
+++ Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD4/index.php
@@ -5,11 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data i czas</title>
+    <style>
+        * {
+            font-family: 'Arial';
+            text-align: center;
+            color: white;
+            padding: 15px;
+            background-color: rgb(19,19,19);
+        }
+    </style>
 </head>
 <body>
     <?php  
         header("refresh: 1");
-        echo '<h2>Teraz jest '.date('H:i:s').'</h2>';
+        echo '<h1 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h1>';
     ?>
 </body>
 </html>
