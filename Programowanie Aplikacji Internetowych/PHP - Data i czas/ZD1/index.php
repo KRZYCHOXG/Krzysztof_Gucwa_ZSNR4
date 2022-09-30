a45769634f658dd02aa1a8f2f160afeac674c890
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 07:50:59 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data i czas</title>
+</head>
+<body>
+    <?php  
+        echo date('2016/10/22').'<br>';
+    ?>
+</body>
+</html>
