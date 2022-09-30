789ea46321dba31c1b3894385111b9ef322b74cc
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-09-30 08:38:03 +0200

    Create index.php

---
+++ Programowanie Aplikacji Internetowych/PHP - Data i czas/ZD5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data i czas</title>
+    <style>
+        * {
+            font-family: 'Arial';
+            text-align: center;
+            color: white;
+            padding: 15px;
+            background-color: rgb(19,19,19);
+        }
+    </style>
+</head>
+<body>
+    <?php  
+        header("refresh: 1");
+        echo '<h1 style="text-transform:uppercase;">Teraz jest '.date('H:i:s').'</h1>';
+        echo '<h1 style="text-transform:uppercase;">Miesiąc: '.date('F').'</h1>';
+    ?>
+</body>
+</html>
