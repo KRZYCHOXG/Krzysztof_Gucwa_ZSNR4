144cb64c103c48177aa4f291c63fff602a114441
Krzysztof Gucwa <90255059+KiT4S@users.noreply.github.com>
2022-11-21 20:23:40 +0100

    Add files via upload

---
+++ Programowanie Aplikacji Internetowych/PHP - Apl/baza.php
@@ -0,0 +1,6 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "kedzior";
+?>
---
+++ Programowanie Aplikacji Internetowych/PHP - Apl/wyloguj.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    
+    unset($_SESSION['user']);
+    unset($_SESSION['error']);
+
+    session_destroy();
+    header("Location: index.php");
+?>
